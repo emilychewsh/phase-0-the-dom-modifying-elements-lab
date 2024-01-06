3d3d25cea35e6eef3576822fa6c818ef00b6ec71
Emily Chew <emilychewsh@gmail.com>
2024-01-06 15:51:05 +1100

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code here!
+const main = document.querySelector("#main")
+main.remove()
+
+const newHeader = document.createElement("h1")
+newHeader.id = "victory"
+newHeader.textContent = "GUTS-from-BERSERK is the champion"
